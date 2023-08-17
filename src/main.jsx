9987ee3810a26cc999990d4a48d1fe7aed42f267
Eray Karakaşlı <eraycode01@gmail.com>
2023-08-17 15:52:48 +0300

    last commite

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-import { Provider } from 'react-redux'
-import { store } from './redux/store.js'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
+  <React.StrictMode>
     <App />
-  </Provider>,
+  </React.StrictMode>,
 )
